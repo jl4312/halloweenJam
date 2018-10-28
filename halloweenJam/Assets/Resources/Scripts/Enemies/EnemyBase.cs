e257a2c25b7449af914b231c25b3bf45ab237bc7
mike4ruls <mike5ruls@gmail.com>
2018-10-28 08:20:59 -0400

    Created new witch type prefabs

--- halloweenJam/Assets/Resources/Scripts/Enemies/EnemyBase.cs
+++ halloweenJam/Assets/Resources/Scripts/Enemies/EnemyBase.cs
@@ -52,6 +52,7 @@ namespace MisfitMakers
             {
                 if (!hasReachedTarget)
                 {
+                    FindNearestTower();
                     SeekStructure();
                 }
                 else
