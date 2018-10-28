f0407c1f8ba4763ed5a1b358bdab4b5e8edf0f92
Jo <jl4312@rit.edu>
2018-10-27 23:22:58 -0400

    Initial Structure script

---
+++ halloweenJam/Assets/Resources/Scripts/Structure/StructureBase.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureBase : MonoBehaviour {
+
+	[Header("Structure Base Setting")]
+	public float health;
+	public float buildTime;
+	public float cost;
+	public bool alive;
+	public bool building = true;
+
+	public StructureBase(float health, float buildTime, float cost){
+		this.health = health;
+		this.buildTime = buildTime;
+		this.cost = cost;
+		alive = true;
+		building = true;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (alive) {
+			
+		}
+
+		if (building)
+			Build ();
+		
+	}
+
+	public void TakeDamage(float damage)
+	{
+		health -= damage;
+		if (health <= 0) {
+			alive = false;
+			this.gameObject.SetActive(false);
+		}
+	}
+
+	public void Build()
+	{
+		buildTime -= Time.deltaTime;
+
+		if(buildTime <= 0){
+			building = false;
+		}
+	}
+
+
+
+
+
+
+}
---
+++ halloweenJam/Assets/Resources/Scripts/Structure/StructureTurrent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureTurrent : StructureBase {
+
+
+	private List<GameObject> enemyList;
+
+	private GameObject closestEnemy;
+
+	public StructureTurrent(float health, float buildTime, float cost) : base(health, buildTime, cost)
+	{
+		enemyList = new List<GameObject> ();
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (alive) {
+			
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (!alive) {
+		}
+
+		if (other.tag == "Enemy") {
+			enemyList.Add(other.gameObject);
+
+		}
+	}
+
+
+
+}
