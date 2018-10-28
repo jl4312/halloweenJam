0d04a123f8451002f8a48cc5cd79eef31b7f91f3
Jo <jl4312@rit.edu>
2018-10-28 00:35:15 -0400

    Turrent Basics Starting script for projectiles

--- halloweenJam/Assets/Resources/Scripts/Structure/StructureTurrent.cs
+++ halloweenJam/Assets/Resources/Scripts/Structure/StructureTurrent.cs
@@ -8,18 +8,20 @@ namespace MisfitMakers
     {
 
         private bool canAttack = false;
-        private List<GameObject> enemyList;
+		private float coolDown = 1f;
 
-        private GameObject closestEnemy;
+
+        private List<GameObject> enemyList;
+		private GameObject closestEnemy;
 
         public StructureTurrent(float health, float buildTime, float cost) : base(health, buildTime, cost)
         {
-            enemyList = new List<GameObject>();
+            
         }
         // Use this for initialization
         void Start()
         {
-
+			enemyList = new List<GameObject>();
         }
 
         // Update is called once per frame
@@ -27,6 +29,7 @@ namespace MisfitMakers
         {
             if (!isDead)
             {
+				UpdateEnemyList ();
 
             }
         }
@@ -44,13 +47,49 @@ namespace MisfitMakers
             }
         }
 
+		void OnTriggerExit(Collider other)
+		{
+			if (other.tag == "Enemy") {
+				enemyList.Remove(other.gameObject);
+			}
+		}
+		void UpdateEnemyList()
+		{
+			float closetEnemyDist;
+			Vector3 dist;
+
+			closetEnemyDist = 100000f; 
+
+			for (int i = 0; i < enemyList.Count; i++) {
+
+				dist = enemyList[i].transform.position - this.transform.position;
+
+				if(dist.magnitude < closetEnemyDist)
+				{
+					closetEnemyDist = dist.magnitude;
+					closestEnemy = enemyList[i];
+				}
+
+				if(enemyList[i].GetComponent<EnemyBase>().isDead)
+					enemyList.RemoveAt (i);
+
+
+			}
+		}
         public IEnumerator AttackCD(float time)
         {
             yield return new WaitForSeconds(time);
             canAttack = true;
         }
 
+		public void Attack()
+		{
+			if (closestEnemy && canAttack) {
 
+				canAttack = false;
+				AttackCD (coolDown);
+			}
+		}
 
     }
 }
